75f1f100ba786d01a5ba75b842afa0727aa59a6b
adlaylatte <dgs2288@naver.com>
2025-05-13 21:56:17 +0900

    feat(user): user repository 구현 #3
    - Layer 분리 목적, User Domain 기능 명세를 위한 UserRepository Interface 생성
    - UserRepositoryImpl Class 생성 및 UserRepository 메서드 구현
    - UserJpaRepository Interface 생성
    - username 중복 여부를 판단하기 위한 UserRepository - isExistUsername() 구현
    - UserRepositoryImpl test 작성

---
+++ src/main/java/com/example/internassignment/infrastructure/UserRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.example.internassignment.infrastructure;
+
+import com.example.internassignment.domain.UserRepository;
+import com.example.internassignment.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public Optional<User> findByUserId(Long userId) {
+        return userJpaRepository.findById(userId);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userJpaRepository.findByUsername(username);
+    }
+
+    @Override
+    public boolean isExistUsername(String username) {
+        return userJpaRepository.existsByUsername(username);
+    }
+}
