605b1baf0e8b5c48f4e6432ccf1d7f21b6a45065
adlaylatte <dgs2288@naver.com>
2025-05-15 00:57:31 +0900

    feat(user): Update Role Security config #22
    - 권한별 api endpoint security config 수정
    - jwt security filter 구현
    - filter test code 작성

--- src/main/java/com/example/internassignment/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/example/internassignment/infrastructure/config/SecurityConfig.java
@@ -1,32 +1,49 @@
 package com.example.internassignment.infrastructure.config;
 
+import com.example.internassignment.infrastructure.jwt.JwtTokenProvider;
+import com.example.internassignment.infrastructure.security.TokenAuthenticationFilter;
+import com.example.internassignment.infrastructure.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtTokenProvider provider;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(AbstractHttpConfigurer::disable);
         http.formLogin(AbstractHttpConfigurer::disable);
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable)
+                .headers((headersConfigurer)->headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
-        // TODO : API별 권한 설정 및 auth 설정
         http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login","/signup").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
-//                .requestMatchers("/api/v1/users/**").permitAll()
         );
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter () {
+        return new TokenAuthenticationFilter(provider, userDetailsService);
     }
 
     @Bean
